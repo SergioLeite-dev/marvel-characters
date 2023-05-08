22489f7c96b973f78b68d9baff4a2f13b5743090
Sergio Leite <sergioleitedev@gmail.com>
2023-05-08 07:51:09 -0300

    ✨ feat: Character Details Page. Added Hero animation to and from Details page.

--- marvel_characters/lib/application/route_manager.dart
+++ marvel_characters/lib/application/route_manager.dart
@@ -1,15 +1,25 @@
 import 'package:get/get.dart';
+import 'package:marvel_characters/models/character_model.dart';
+import 'package:marvel_characters/modules/details/view/details_page.dart';
 import 'package:marvel_characters/modules/home/home_bindings.dart';
 import 'package:marvel_characters/modules/home/view/home_page.dart';
 
 class RouteManager {
   static String home = "/";
+  static String details = "/details";
 
   static List<GetPage> routes = [
     GetPage(
-      name: '/',
+      name: home,
       binding: HomeBindings(),
       page: () => HomePage(Get.find()),
+    ),
+    GetPage(
+      name: details,
+      page: () {
+        Character character = Get.arguments;
+        return DetailsPage(character);
+      },
     ),
   ];
 }
--- marvel_characters/lib/modules/home/view/widgets/character_card.dart
+++ marvel_characters/lib/modules/home/view/widgets/character_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:marvel_characters/application/route_manager.dart';
 import 'package:marvel_characters/models/character_model.dart';
 
 class CharacterCard extends StatelessWidget {
@@ -12,47 +14,55 @@ class CharacterCard extends StatelessWidget {
     if (character?.thumbnail?.path != null && character?.thumbnail?.extension != null) {
       thumbPath = "${character!.thumbnail!.path!}.${character!.thumbnail!.extension!}";
     }
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(width: 3),
-        borderRadius: BorderRadius.circular(40),
-      ),
-      child: ClipRRect(
-        borderRadius: const BorderRadius.all(Radius.circular(40)),
-        child: Stack(
-          children: [
-            Align(
-              alignment: Alignment.center,
-              child: SizedBox(
-                width: 200,
-                height: 200,
-                child: thumbPath != null
-                    ? Image.network(
-                        thumbPath,
-                        fit: BoxFit.cover,
-                      )
-                    : const Placeholder(),
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                width: 200,
-                decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.85),
-                ),
-                child: Text(
-                  character?.name ?? "",
-                  textAlign: TextAlign.center,
-                  maxLines: 1,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
+    return GestureDetector(
+      onTap: () {
+        Get.toNamed(RouteManager.details, arguments: character);
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(width: 3),
+          borderRadius: BorderRadius.circular(40),
+        ),
+        child: Hero(
+          tag: character!.id!,
+          child: ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(40)),
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                    width: 200,
+                    height: 200,
+                    child: thumbPath != null
+                        ? Image.network(
+                            thumbPath,
+                            fit: BoxFit.cover,
+                          )
+                        : const Placeholder(),
                   ),
                 ),
-              ),
-            )
-          ],
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    width: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.85),
+                    ),
+                    child: Text(
+                      character?.name ?? "",
+                      textAlign: TextAlign.center,
+                      maxLines: 1,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
