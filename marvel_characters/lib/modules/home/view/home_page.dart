896bc6e6a0c3e9fb7e165ab090853fcec3aa5f5c
Sergio Leite <sergioleitedev@gmail.com>
2023-05-12 17:52:26 -0300

    ✨ feat: Loading more characters animation.

--- marvel_characters/lib/modules/home/view/home_page.dart
+++ marvel_characters/lib/modules/home/view/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:marvel_characters/modules/home/controller/home_controller.dart';
 import 'package:marvel_characters/modules/home/view/widgets/characters_list_view.dart';
 import 'package:marvel_characters/modules/home/view/widgets/get_to_favorites_button.dart';
@@ -30,6 +31,17 @@ class HomePage extends StatelessWidget {
             ),
             const SizedBox(height: 10),
             const Expanded(child: CharactersListView()),
+            GetBuilder<HomeController>(builder: (_) {
+              if (_controller.isLoading) {
+                return Container(
+                  margin: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+                  height: 20,
+                  width: 20,
+                  child: const CircularProgressIndicator(),
+                );
+              }
+              return const SizedBox();
+            })
           ],
         ),
       ),
